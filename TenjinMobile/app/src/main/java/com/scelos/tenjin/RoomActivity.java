c91ce94e2c59272f4689ec176d72226c975a9baa
Cameron Wheeler <cameron@scelos.com>
2015-07-27 16:35:49 -0400

    Mobile app fixes, feature complete for the moment.

--- TenjinMobile/app/src/main/java/com/scelos/tenjin/RoomActivity.java
+++ TenjinMobile/app/src/main/java/com/scelos/tenjin/RoomActivity.java
@@ -1,5 +1,6 @@
 package com.scelos.tenjin;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,8 +13,13 @@ import com.larswerkman.holocolorpicker.ColorPicker;
 import com.larswerkman.holocolorpicker.SaturationBar;
 import com.larswerkman.holocolorpicker.ValueBar;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 
-public class RoomActivity extends ActionBarActivity {
+
+public class RoomActivity extends ActionBarActivity implements LightControllerDelegate {
+    private final int lightOn = 255;
+
     private ColorPicker picker;
     private SaturationBar saturationBar;
     private ValueBar valueBar;
@@ -25,6 +31,46 @@ public class RoomActivity extends ActionBarActivity {
     private LightController lc;
 
     @Override
+    public void lightControllerContextUpdated(HashMap context) {
+        leftBed.setEnabled(true);
+        rightBed.setEnabled(true);
+        desk.setEnabled(true);
+        picker.setEnabled(true);
+        saturationBar.setEnabled(true);
+        valueBar.setEnabled(true);
+
+        if ((int)context.get("white3") == lightOn) {
+            desk.setChecked(true);
+        }
+
+        if ((int)context.get("white1") == lightOn && (int)context.get("red1") == lightOn && (int)context.get("green1") == lightOn && (int)context.get("blue1") == lightOn) {
+            leftBed.setChecked(true);
+        }
+
+        if ((int)context.get("white2") == lightOn && (int)context.get("red2") == lightOn && (int)context.get("green2") == lightOn && (int)context.get("blue2") == lightOn) {
+            rightBed.setChecked(true);
+        }
+
+        picker.setColor(Color.argb(1, ((int)context.get("red1")+(int)context.get("red2"))/2, ((int)context.get("green1")+(int)context.get("green2"))/2, ((int)context.get("blue1")+(int)context.get("blue2"))/2));
+    }
+
+    @Override
+    public void lightControllerProxyAuthSuccess() {
+        lc.fetchContext(this);
+    }
+
+    @Override
+    public void lightControllerProxyAuthFailure() {
+        returnToLogin();
+    }
+
+    @Override
+    protected void onResume() {
+        lc = new LightController(this, "http://192.168.1.126:4000/", this.getIntent().getStringExtra("username"), this.getIntent().getStringExtra("password"));
+        super.onResume();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
@@ -34,8 +80,17 @@ public class RoomActivity extends ActionBarActivity {
         valueBar = (ValueBar)findViewById(R.id.valuebar);
 
         leftBed = (Switch)findViewById(R.id.leftBed);
-        rightBed = (Switch)findViewById(R.id.leftBed);
+        rightBed = (Switch)findViewById(R.id.rightBed);
         desk = (Switch)findViewById(R.id.mainDesk);
+
+        leftBed.setEnabled(false);
+        rightBed.setEnabled(false);
+        desk.setEnabled(false);
+        picker.setEnabled(false);
+        saturationBar.setEnabled(false);
+        valueBar.setEnabled(false);
+
+        lc = new LightController(this, "http://192.168.1.126:4000/", this.getIntent().getStringExtra("username"), this.getIntent().getStringExtra("password"));
 
         picker.addValueBar(valueBar);
         picker.addSaturationBar(saturationBar);
@@ -70,21 +125,45 @@ public class RoomActivity extends ActionBarActivity {
         leftBed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                try {
+                    if (isChecked) {
+                        lc.setLight(LightController.superWhite1, lightOn);
+                    } else {
+                        lc.setLight(LightController.superWhite1, 0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         rightBed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                try {
+                    if (isChecked) {
+                        lc.setLight(LightController.superWhite2, lightOn);
+                    } else {
+                        lc.setLight(LightController.superWhite2, 0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         desk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                try {
+                    if (isChecked) {
+                        lc.setLight(LightController.whiteMain, lightOn);
+                    } else {
+                        lc.setLight(LightController.whiteMain, 0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -98,13 +177,8 @@ public class RoomActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_logout) {
+            returnToLogin();
             return true;
         }
 
@@ -112,6 +186,20 @@ public class RoomActivity extends ActionBarActivity {
     }
 
     private void pickerChanged() {
-        System.out.println(Color.red(picker.getColor()) + " " + Color.green(picker.getColor()) + " " + Color.blue(picker.getColor()));
+        try {
+            lc.setLight(LightController.blue, Color.blue(picker.getColor()));
+            lc.setLight(LightController.red, Color.red(picker.getColor()));
+            lc.setLight(LightController.green, Color.green(picker.getColor()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void returnToLogin() {
+        Intent i = new Intent(this.getApplicationContext(), LoginActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        this.startActivity(i);
+
+        finish();
     }
 }
