a5c27cd728cecd2231e9605120f0b13236d28682
James Sullivan <james@scelos.com>
2015-08-24 17:31:40 -0400

    Finished NPR and Weekly Weather, added backgrounds

--- Tenjin Service/app/controllers/npr.js
+++ Tenjin Service/app/controllers/npr.js
@@ -1,11 +1,20 @@
 tenjin.controller('nprController', function($scope, $location, $http) {
-	$http.get('http://api.npr.org/query?output=JSON&numResults=20&apiKey=MDE1NzM4ODYyMDE0MDcxNzIyOTZlYWQxZQ001')
+	$http.get('/cache/npr/')
 	    .then(function(res){
 	    	$scope.stories = [];
+	    	var cur = 0;
 	    	for (var i = 0; i < 20; i++){
 	    		if("image" in res.data.list.story[i]){
-	    			res.data.list.story[i].teaser.$text = String(res.data.list.story[i].teaser.$text).replace(/<[^>]+>/gm, '');
-	    			$scope.stories.push(res.data.list.story[i]);
+	    			if (cur % 2 == 1){
+						res.data.list.story[i].teaser.$text = String(res.data.list.story[i].teaser.$text).replace(/<[^>]+>/gm, '');
+		    			$scope.stories[Math.floor(cur/2)].storytwo = res.data.list.story[i];
+		    			console.log($scope.stories);
+		    			cur++;
+	    			}else{
+		    			res.data.list.story[i].teaser.$text = String(res.data.list.story[i].teaser.$text).replace(/<[^>]+>/gm, '');
+		    			$scope.stories.push(res.data.list.story[i]);
+		    			cur++;
+	    			}
 	    		}
 	    	}
 	});
